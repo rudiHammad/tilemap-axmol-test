eb054611186b4b02063f7258b79948cc9abde2da
rudiHammad <rudihammad@gmail.com>
2025-01-21 14:33:51 +0100

    Delete Content/tilesets directory

--- Content/tilesets/myTileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="myTileset" tilewidth="24" tileheight="24" tilecount="8" columns="4">
- <image source="myTileset.png" width="96" height="48"/>
-</tileset>
