3eb47e5cc04a3d39baf70d4122ba9e43e0cfd100
rudiHammad <rudihammad@gmail.com>
2025-01-21 14:34:37 +0100

    Add files via upload

---
+++ Content/tilesets/myTileset.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="myTileset" tilewidth="24" tileheight="24" tilecount="8" columns="4">
+ <image source="myTileset.png" width="96" height="48"/>
+ <tile id="5">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="1000"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
